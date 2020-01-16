b93e81e591c4fc5954b9014de1ad1bec9bd7089d
Suleiman Yunus <kayangasuleiman@gmail.com>
2020-01-16 14:05:16 +0300

    Updates

---
+++ precache-manifest.236c91da946902cd981c6907311b6581.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ade5de16ea01377ae13b2a378da966",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "79879b31ce32cbdf7e8b",
+    "url": "/monsters-rolodex/static/css/main.4cb18e8e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79879b31ce32cbdf7e8b",
+    "url": "/monsters-rolodex/static/js/main.7c675ed9.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
